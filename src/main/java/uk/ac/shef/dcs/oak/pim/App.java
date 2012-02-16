4f879188c618dc601b772de2068e106b655e030c
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2012-02-16 09:44:13 +0000

    Init commit

---
+++ src/main/java/uk/ac/shef/dcs/oak/pim/App.java
@@ -0,0 +1,13 @@
+package uk.ac.shef.dcs.oak.pim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
